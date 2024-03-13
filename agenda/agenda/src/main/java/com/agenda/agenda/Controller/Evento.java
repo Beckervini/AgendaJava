5725bf4e105f94d16a0ff06c9cb1db0a4a893499
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-13 10:58:26 -0300

    commit

--- agenda/agenda/src/main/java/com/agenda/agenda/Controller/Evento.java
+++ agenda/agenda/src/main/java/com/agenda/agenda/Controller/Evento.java
@@ -1,9 +1,40 @@
 package com.agenda.agenda.Controller;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-@EntityScan
+@Entity
 public class Evento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String descricao;
     private String data;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
 }
